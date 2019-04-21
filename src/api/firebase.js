249ceddbb13bbeecff5b8d9b2135a79895fdbd9e
eltim12 <43396839+eltim12@users.noreply.github.com>
2019-04-21 23:47:03 +0700

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyDvPLo3LBMduG6zP2sIbMsTTmV6H9WegGc",
-    authDomain: "kanban-1205.firebaseapp.com",
-    databaseURL: "https://kanban-1205.firebaseio.com",
-    projectId: "kanban-1205",
-    storageBucket: "kanban-1205.appspot.com",
-    messagingSenderId: "617412265725"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
